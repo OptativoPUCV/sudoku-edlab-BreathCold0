716ebef8114eec4f510a42c16d87fcc5489c0310
repl.it user <replituser@example.com>
2020-10-17 22:59:28 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -88,7 +88,25 @@ int is_final(Node* n){
 }
 
 Node* DFS(Node* initial, int* cont){
+  Stack* S=createStack();
+  push(S,initial);
+  while (get_size(S) != 0) {
+    
+    Node * n = top(S);
+    pop(S);
+    if (is_final(n)) {
+      return n;
+    }
+    List * adj = get_adj_nodes(n);    
+    //Node * aux = first(n);
+    while (adj != NULL) {
+      push(S,adj);
+      adj=next(adj);
+    }
+    free(n);
+  }
   return NULL;
+
 }
 
 
