ec0bb3665e5884d11917940e5cda43f3f8153480
repl.it user <replituser@example.com>
2020-10-18 16:46:51 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -45,6 +45,7 @@ void print_node(Node* n){
 int is_valid(Node* n){
   Node * aux = n;
   int i,j;
+  if (n != NULL) {
   for (i = 0; i < 9; i++) {
     for (j = 0; j < 9; j++) {
       int k;
@@ -92,6 +93,7 @@ int is_valid(Node* n){
         }
       }
     }
+  }
   }
     return 1;
 }
